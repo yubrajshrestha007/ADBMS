287bf747ab103ae48a5eee4cc5033140f154c3ba
yubrajshrestha007 <yuvishrestha0727@gmail.com>
2025-07-07 16:02:57 +0545

    first commit

--- lab2.sql
+++ lab2.sql
@@ -1,20 +1,32 @@
-DELIMITER $$
-
-CREATE PROCEDURE ApplyMarkupToCategory(
-    IN markup DECIMAL(10,2),
-    IN categoryId INT,
-    OUT totalItems INT
-)
+CREATE PROCEDURE dbo.ApplyMarkupToCategory
+    @Markup DECIMAL(10, 2),         -- Input: Amount to add to price
+    @CategoryId INT,                -- Input: Category to update
+    @TotalItems INT OUTPUT          -- Output: Total items updated
+AS
 BEGIN
-    -- Apply the markup to all products in the given category
+    SET NOCOUNT ON;
+
+    -- Step 1: Apply the markup to all products in the given category
     UPDATE Product
-    SET Price = Price + markup
-    WHERE ProductCategoryRef_Id = categoryId;
+    SET Price = Price + @Markup
+    WHERE ProductCategoryRef_Id = @CategoryId;
 
-    -- Count total products in that category after update
-    SELECT COUNT(*) INTO totalItems
+    -- Step 2: Count how many products were updated
+    SELECT @TotalItems = COUNT(*)
     FROM Product
-    WHERE ProductCategoryRef_Id = categoryId;
-END$$
+    WHERE ProductCategoryRef_Id = @CategoryId;
+END;
 
-DELIMITER ;
+
+
+-- Declare a variable to store the output
+DECLARE @Count INT;
+
+-- Execute the procedure
+EXEC dbo.ApplyMarkupToCategory
+    @Markup = 100,
+    @CategoryId = 2,
+    @TotalItems = @Count OUTPUT;
+
+-- View the result
+SELECT @Count AS NumberOfItems;
